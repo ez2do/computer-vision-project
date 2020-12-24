e363a8d6878550d68263ea210e8f8f351b2d7ab8
thanhdn <daongocthanh98hy@gmail.com>
2020-12-24 22:05:54 +0700

    update ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -8,8 +8,10 @@ function App() {
   const result = useRef();
   const upload = useRef();
   const [option, setOption] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   async function onSubmit() {
+    setLoading(true);
     const dataURL = image.current.toDataURL("image/jpeg", 1.0);
     const blobBin = atob(dataURL.split(",")[1]);
     const array = [];
@@ -39,6 +41,7 @@ function App() {
     };
 
     img.src = URL.createObjectURL(blob);
+    setLoading(false);
   }
 
   function onUpload(e) {
@@ -51,8 +54,15 @@ function App() {
     img.src = URL.createObjectURL(e.target.files[0]);
   }
 
+  function onDownload() {
+    const link = document.createElement("a");
+    link.download = "image.png";
+    link.href = result.current.toDataURL();
+    link.click();
+  }
+
   return (
-    <div className="h-screen bg-gray-200">
+    <div className="h-screen flex bg-gray-200">
       <div className="flex items-center">
         <div className="mt-10 ml-10">
           <TrainClass canvasRef={image} upload={upload} />
@@ -68,10 +78,16 @@ function App() {
           option={option}
           setOption={setOption}
           onSubmit={onSubmit}
+          loading={loading}
         />
         <div className="m-4 bg-white rounded-md p-2">
-          <div className="border-b-2 border-solid border-gray-400 p-2 text-lg font-bold">
-            Result
+          <div className="flex justify-between items-center border-b-2 border-solid border-gray-400 p-2 text-lg font-bold">
+            <div>Result</div>
+            <button
+              onClick={onDownload}
+              className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white p-1 px-4 rounded"
+              children="download image"
+            />
           </div>
           <canvas ref={result} style={{ width: 300, height: 300 }} />
         </div>
--- ui/src/components/train_options.js
+++ ui/src/components/train_options.js
@@ -1,6 +1,6 @@
-function TrainOptions({ option, setOption, onSubmit }) {
+function TrainOptions({ option, setOption, onSubmit, loading }) {
   return (
-    <div className="m-4 bg-white rounded-md p-2">
+    <div className="m-4 bg-white rounded-md p-2 w-48">
       <div className="border-b-2 border-solid border-gray-400 p-2 text-lg font-bold">
         Training Options
       </div>
@@ -45,8 +45,36 @@ function TrainOptions({ option, setOption, onSubmit }) {
         </div>
       </div>
       <button
-        className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white p-1 px-16 rounded"
-        children="Capture"
+        disabled={loading}
+        className="cursor-pointer w-full bg-blue-500 hover:bg-blue-700 text-white p-1 px-16 rounded"
+        children={
+          loading ? (
+            <div className="flex justify-center">
+              <svg
+                className="animate-spin h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            </div>
+          ) : (
+            "Start"
+          )
+        }
         onClick={onSubmit}
       />
     </div>
